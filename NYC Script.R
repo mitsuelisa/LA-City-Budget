e684961d54049040064eb80d0826b502f1b95c0b
mitsuelisa <mitsuelisa@gmail.com>
2019-09-20 01:13:55 -0400

    done

--- NYC Script.R
+++ NYC Script.R
@@ -3,8 +3,29 @@ library(readxl)
 library(readr)
 library(janitor)
 library(dplyr)
+library(tidyr)
 
-NYC <- read_excel("data/NYC.xlsx") %>% 
-  clean_names()
+NYC <- read_excel("data/NYC_sectors2.xlsx") %>% clean_names()
 
-NYC
+view(NYC)
+
+#ggplot(NYC, aes(x = sectors, y = ependiture, fill = sectors)) + 
+#geom_col() +
+#facet_wrap(~year) 
+
+#ggplot(NYC,
+#  aes(x = year, y = ependiture, fill = sectors)) + 
+# geom_col(position = 'fill')+
+# theme_minimal()+
+#labs(title = "NYC")+
+# xlab("Year")+
+# ylab("Percentage of total expenditures")
+
+ggplot(NYC, aes(x = year, y = ependiture, fill = sectors)) + 
+  geom_col()+
+  theme_minimal()+
+  labs(title = "NYC")+
+  xlab("Year")+
+  ylab("Expenditures")
+
+
